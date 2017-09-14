f73419613477312cfcf7c61cdfd6eb996b7ea8a3
Edward Schults <edward.schults@gmail.com>
2017-09-14 19:45:32 +0200

    added jsx to index

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,12 @@
 module.exports = {
-  entry: './src/index.js',
+  entry: './src/index.jsx',
   output: {
     filename: 'dist/bundle.js'
   },
   devtool: 'sourcemap',
   module: {
     loaders: [{
-      test: /\.js$/,
+      test: /\.jsx?$/,
       exclude: /node_modules/,
       loader: 'babel-loader'
     }]
