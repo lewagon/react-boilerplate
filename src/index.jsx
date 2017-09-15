fc6a72b0e6dfdaef95035b7a0b5dccb1bcce444c
Edward Schults <edward.schults@gmail.com>
2017-09-15 14:40:39 +0200

    eslint + refacto

--- src/index.jsx
+++ src/index.jsx
@@ -2,10 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 // A simple example of a functional component
-const Hello = (props) => {
-  // Looks like HTML but is JSX
-  return <h1>Hello, {props.name}</h1>;
-}
+const Hello = props => <h1>Hello, {props.name}</h1>;
 
 // render an instance of the component in the DOM
 ReactDOM.render(<Hello name="Boris" />, document.querySelector('.container'));
