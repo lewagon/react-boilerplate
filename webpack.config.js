9132e57b117cb6126647c699f276484e794a79cd
Sébastien Saunier <seb@saunier.me>
2017-09-26 10:46:53 +0200

    Typo in dist

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ const path = require('path');
 module.exports = {
   entry: './src/index.jsx',
   output: {
-    path: path.resolve(__dirname, '/dist'),
+    path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js'
   },
   plugins: [
